6ad8149385df6b238d93dd6a5d9397f4f57a4df0
tuacy <1007178106@qq.com>
2018-01-11 17:02:01 +0800

    修改表格

--- app/src/main/java/com/tuacy/layoutmanagerdemo/table/TableItemDecoration.java
+++ app/src/main/java/com/tuacy/layoutmanagerdemo/table/TableItemDecoration.java
@@ -7,7 +7,6 @@ import android.graphics.Canvas;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
 
 public class TableItemDecoration extends RecyclerView.ItemDecoration {
